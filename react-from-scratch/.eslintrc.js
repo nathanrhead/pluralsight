4d8488906a858479b03fef3fc35f659d3d555ae9
Nathan Rhead Cox <nrc4@hotmail.com>
2021-09-27 16:20:54 -0700

    Add react-from-scratch app, which was a tutorial to learn how to install the basics needed for react to work.

---
+++ react-from-scratch/.eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  parser: "babel-eslint",
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    ecmaFeatures: {
+      impliedStrict: true,
+      jsx: true,
+    },
+    sourceType: "module",
+  },
+  plugins: ["react", "react-hooks"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier"
+  ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    // You can do your customizations here...
+    'react/prop-types': ['off']
+  },
+};
